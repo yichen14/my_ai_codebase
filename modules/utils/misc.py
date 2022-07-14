7c5f2e038770d9124ee93582c86621e9b85608b3
yichen14 <yichen14@illinois.edu>
2022-07-13 22:13:04 -0500

    now args can override cfg

--- modules/utils/misc.py
+++ modules/utils/misc.py
@@ -2,16 +2,23 @@ import torch
 import os
 import urllib
 
+DATASET_ROOT = "/home/randomgraph/data"
+
+def set_dataset_root(path):
+    global DATASET_ROOT
+    DATASET_ROOT = path
+
 def download_file(url, local_path):
     g = urllib.request.urlopen(url)
     with open(local_path, 'b+w') as f:
         f.write(g.read())
 
 def get_dataset_root():
+    global DATASET_ROOT
     try:
         return os.environ['DATASET_ROOT']
     except KeyError:
-        return "/home/randomgraph/data"
+        return DATASET_ROOT
 
 def guess_device():
     if torch.cuda.is_available():
