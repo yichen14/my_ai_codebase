446670ae08f21be4ff0f20cf04d42878c5187179
yichen14 <yichen14@illinois.edu>
2022-07-19 01:58:54 -0500

    debug GAE/VGAE

--- modules/trainer/autoencoder_trainer.py
+++ modules/trainer/autoencoder_trainer.py
@@ -25,7 +25,7 @@ class autoencoder_trainer(base_trainer):
         for epoch in pbar:
             self.optimizer.zero_grad()
             z = self.model.encode(self.static_data.feat_static_train.to(self.device), self.static_data.edge_idx_train.to(self.device))
-            loss = self.model.recon_loss(z, self.static_data.pos_edges_l_static_train.to(self.device),self.static_data.neg_edges_l_static_train.to(self.device))
+            loss = self.model.recon_loss(z, self.static_data.edge_idx_train.to(self.device))
             if self.cfg.MODEL.model == "VGAE":
                 loss = loss + (1 / self.static_data.feat_static_train.shape[0]) * self.model.kl_loss()
             loss.backward()
@@ -34,10 +34,13 @@ class autoencoder_trainer(base_trainer):
                 self.inference([self.static_data.feat_static_val.to(self.device), self.static_data.feat_static_test.to(self.device)],
                         [self.static_data.edge_idx_val.to(self.device), self.static_data.edge_idx_test.to(self.device)],
                         [self.static_data.adj_dense_merge_val, self.static_data.adj_dense_merge_test],
-                        [self.static_data.pos_edges_l_static_val.to(self.device), self.static_data.pos_edges_l_static_test.to(self.device)],
-                        [self.static_data.neg_edges_l_static_val.to(self.device), self.static_data.neg_edges_l_static_test.to(self.device)])
+                        [self.static_data.pos_edges_l_static_val.to(self.device).T, self.static_data.pos_edges_l_static_test.to(self.device).T],
+                        [self.static_data.neg_edges_l_static_val.to(self.device).T, self.static_data.neg_edges_l_static_test.to(self.device).T])
                 pbar.set_description('Epoch {}/{}, Loss {:.3f}, Test AUC {:.3f}, Test AP {:.3f}, Time {:.1f}s'.format(epoch, self.max_epochs, loss.item(),self.cal_metric.test_metrics["AUC"], 
                     self.cal_metric.test_metrics["AP"], time.time() - start_time))
+        logging.info("Best performance: Test AUC {:.3f}, Test AP {:.3f}, Val AUC {:.3f}, Val AP {:.3f}".format(
+            self.cal_metric.best_test_metrics["AUC"], self.cal_metric.best_test_metrics["AP"], self.cal_metric.best_val_metrics["AUC"], self.cal_metric.best_val_metrics["AP"]))
+
         return self.cal_metric.best_test_metrics["AUC"], self.cal_metric.best_test_metrics["AP"]
 
     def train_one(self, device):
@@ -65,8 +68,10 @@ class autoencoder_trainer(base_trainer):
 
     @torch.no_grad()
     def inference(self, x_in, edge_idx_list, adj_orig_dense_list, pos_edges_l, neg_edges_l):
+        self.model.eval()
         z_val = self.model.encode(x_in[0], edge_idx_list[0])
         z_test = self.model.encode(x_in[1], edge_idx_list[1])
+        # print(self.model.test(z_test,pos_edges_l[1], neg_edges_l[1]))
         self.cal_metric.update(pos_edges_l
                                 , neg_edges_l
                                 , adj_orig_dense_list
