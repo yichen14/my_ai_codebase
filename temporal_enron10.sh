95e62e6a7419f21cc2ee0e747949dcc8065b527e
baoyul2 <baoyul2@illinois.edu>
2022-07-28 19:46:09 -0500

    Add bash files for lr.

---
+++ temporal_enron10.sh
@@ -0,0 +1,7 @@
+python3 entries/train.py --cfg configs/link_pred_temporal_enron10.yaml --lr 0.01
+python3 entries/train.py --cfg configs/link_pred_temporal_enron10.yaml --lr 0.005
+python3 entries/train.py --cfg configs/link_pred_temporal_enron10.yaml --lr 0.001
+python3 entries/train.py --cfg configs/link_pred_temporal_enron10.yaml --lr 0.0005
+python3 entries/train.py --cfg configs/link_pred_temporal_enron10.yaml --lr 0.0001
+python3 entries/train.py --cfg configs/link_pred_temporal_enron10.yaml --lr 0.00005
+python3 entries/train.py --cfg configs/link_pred_temporal_enron10.yaml --lr 0.00001
